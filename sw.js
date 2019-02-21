44a339cf866bafb0b7515c3aea07e9680164e6d1
Alex Colombo <uricaine@hotmail.com>
2019-02-21 10:13:56 +0100

    service worker

--- sw.js
+++ sw.js
@@ -1,8 +1,6 @@
 self.addEventListener("fetch", event => {
-  if (event.request.url.includes("style")) {
-    event.respondWith(null);
-  } else {
+ 
     event.respondWith(fetch(event.request));
-  }
+  
  // console.log(event);
 });
