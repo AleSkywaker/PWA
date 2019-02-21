1af9fcdf4249d9966f5a79e5ce786c8ccae0dd2a
Alex Colombo <uricaine@hotmail.com>
2019-02-21 11:04:37 +0100

    service worker

--- sw.js
+++ sw.js
@@ -1,19 +1,27 @@
 self.addEventListener("fetch", event => {
- 
-
-   if(event.request.url.includes('.jpg')){
-     console.log(event.request.url);
+  if (event.request.url.includes(".jpg")) {
+    console.log(event.request.url);
 
     //  let fotoReq = fetch('img/main.jpg');
     //  let fotoReq = fetch(event.request.url);
-     let fotoReq = fetch(event.request);
+    let fotoReq = fetch(event.request);
 
-     event.respondWith(fotoReq)
-   }
+    event.respondWith(fotoReq);
+  } else if (event.request.url.includes(".style.css")) {
+    let respuesta = new Response(
+      `
+    body {
+      background-color:red !important;
+      color: blue;
+    }
+    `,
+      {
+        "Content-Type": "text/css"
+      }
+    );
+  }
 
+  event.respondWith(fetch(event.request));
 
-
-    event.respondWith(fetch(event.request));
-  
- // console.log(event);
+  // console.log(event);
 });
