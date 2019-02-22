fad143b586a2b1ecba85e0353c7b26b0c507b66b
Alex Colombo <uricaine@hotmail.com>
2019-02-22 18:06:58 +0100

    service worker

--- sw.js
+++ sw.js
@@ -1,14 +1,13 @@
-self.addEventListener('install', e=>{
-    caches.open('cache-1').then(
-        cache =>{
-            cache.addAll([
-                '/',
-                '/index.html',
-                '/css/styles.css',
-                '/img/main.jpg',
-                'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
-                '/js/app.js'
-            ])
-        }
-    )
-})
+self.addEventListener('install', (e) => {
+	const cacheProm = caches.open('cache-1').then((cache) => {
+		return cache.addAll([
+			'/',
+			'/index.html',
+			'/css/styles.css',
+			'/img/main.jpg',
+			'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+			'/js/app.js'
+		]);
+	});
+	e.waitUntill(cacheProm);
+});
