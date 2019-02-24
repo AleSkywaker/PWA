8c1ad92392219a30929ec8f8b69e5cf62aaba17d
Alex Colombo <uricaine@hotmail.com>
2019-02-24 10:38:20 +0100

    service worker

--- sw.js
+++ sw.js
@@ -1,24 +1,17 @@
-
-const CACHE_STATIC_NAME = 'static-v'
-const CACHE_DYNAMIC_NAME = 'dynamic-v'
-const CACHE_INMUTABLE_NAME = 'inmutable-v'
+const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
 
 self.addEventListener('install', (e) => {
 	const cacheProm = caches.open(CACHE_STATIC_NAME).then((cache) => {
-		return cache.addAll([
-			'/',
-			'/index.html',
-			'/css/style.css',
-			'/img/main.jpg',
-			'/js/app.js'
-		]);
+		return cache.addAll([ '/', '/index.html', '/css/style.css', '/img/main.jpg', '/js/app.js' ]);
 	});
 
-    const cacheInmu = caches.open(CACHE_INMUTABLE_NAME).then((cache)=>{
-		return cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css')
-	})
+	const cacheInmu = caches.open(CACHE_INMUTABLE_NAME).then((cache) => {
+		return cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
+	});
 
-	e.waitUntil(Promise.all(cacheProm,cacheInmu));
+	e.waitUntil(Promise.all(cacheProm, cacheInmu));
 });
 
 self.addEventListener('fetch', (e) => {
@@ -28,15 +21,15 @@ self.addEventListener('fetch', (e) => {
 
 		//No existe el archivo
 		//Tengo que ir a la web
-		// console.log('No existe', e.request.url);
+		console.log('No existe', e.request.url);
 		return fetch(e.request).then((newResp) => {
-			caches.open(CACHE_DYNAMIC_NAME).then(cache=>{
-			    cache.put(e.request, newResp)	
-			})
+			caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
+				cache.put(e.request, newResp);
+			});
 			return newResp.clone();
 		});
 	});
-	e.respondWith(respuesta)
+	e.respondWith(respuesta);
 
 	// 1-Cache Only
 	//e.respondWith(caches.match(e.request))
