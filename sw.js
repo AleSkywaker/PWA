5f57a30618aa321c9c72fb2511304037b38648b3
Alex Colombo <uricaine@hotmail.com>
2019-02-24 10:20:53 +0100

    service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
+const CACHE_NAME = 'cache-1';
+
 self.addEventListener('install', (e) => {
-	const cacheProm = caches.open('cache-V1').then((cache) => {
+	const cacheProm = caches.open(CACHE_NAME).then((cache) => {
 		return cache.addAll([
 			'/',
 			'/index.html',
@@ -9,5 +11,27 @@ self.addEventListener('install', (e) => {
 			'/js/app.js'
 		]);
 	});
+
 	e.waitUntil(cacheProm);
+});
+
+self.addEventListener('fetch', (e) => {
+	//2-Cache with Network Fallback
+	const respuesta = caches.match(e.request).then((res) => {
+		if (res) return res;
+
+		//No existe el archivo
+		//Tengo que ir a la web
+		// console.log('No existe', e.request.url);
+		return fetch(e.request).then((newResp) => {
+			caches.open(CACHE_NAME).then(cache=>{
+			    cache.put(e.request, newResp)	
+			})
+			return newResp.clone();
+		});
+	});
+	e.respondWith(respuesta)
+
+	// 1-Cache Only
+	//e.respondWith(caches.match(e.request))
 });
