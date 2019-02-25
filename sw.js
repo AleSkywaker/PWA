657fd0dfd57e7f7ea1485538e547f4958b9f460c
Alex Colombo <uricaine@hotmail.com>
2019-02-25 18:51:55 +0100

    clean cache

--- sw.js
+++ sw.js
@@ -2,6 +2,19 @@ const CACHE_STATIC_NAME = 'static-v2';
 const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 const CACHE_INMUTABLE_NAME = 'inmutable-v1';
 
+
+function limpiarCache(cacheName, numeroItems){
+   caches.open(cacheName)
+	.then(cache =>{
+		return cache.keys()
+			.then(keys=>{
+				console.log(keys)
+			})
+	})
+}
+
+
+
 self.addEventListener('install', (e) => {
 	const cacheProm = caches.open(CACHE_STATIC_NAME).then((cache) => {
 		return cache.addAll([ '/', '/index.html', '/css/style.css', '/img/main.jpg', '/js/app.js' ]);
