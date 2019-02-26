70be0cfa03bbd80bcb16127a8f8919ee5be23aa2
Alex Colombo <uricaine@hotmail.com>
2019-02-26 20:13:42 +0100

    cache strategy network update

--- sw.js
+++ sw.js
@@ -2,23 +2,17 @@ const CACHE_STATIC_NAME = 'static-v2';
 const CACHE_DYNAMIC_NAME = 'dynamic-v1';
 const CACHE_INMUTABLE_NAME = 'inmutable-v1';
 
+function limpiarCache(cacheName, numeroItems) {
+	caches.open(cacheName).then((cache) => {
+		return cache.keys().then((keys) => {
+			console.log(keys);
 
-function limpiarCache(cacheName, numeroItems){
-   caches.open(cacheName)
-	.then(cache =>{
-		return cache.keys()
-			.then(keys=>{
-				console.log(keys)
-
-				if(keys.length > numeroItems){
-					cache.delete(keys[0])
-						.then(limpiarCache(cacheName, numeroItems))
-				}
-			})
-	})
+			if (keys.length > numeroItems) {
+				cache.delete(keys[0]).then(limpiarCache(cacheName, numeroItems));
+			}
+		});
+	});
 }
-
-
 
 self.addEventListener('install', (e) => {
 	const cacheProm = caches.open(CACHE_STATIC_NAME).then((cache) => {
@@ -29,16 +23,19 @@ self.addEventListener('install', (e) => {
 		return cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
 	});
 
-	e.waitUntil(Promise.all([cacheProm, cacheInmu]));
+	e.waitUntil(Promise.all([ cacheProm, cacheInmu ]));
 });
 
-self.addEventListener('fetch', e => {
-    //4-Cache with Network update
-    //util cuando el rendimiento es critico
-    //los updates siempre estaran un paso atrás.
-	
-	e.respondWith();
+self.addEventListener('fetch', (e) => {
+	//4-Cache with Network update
+	//util cuando el rendimiento es critico
+	//los updates siempre estaran un paso atrás.
+	const respuesta = caches.open(CACHE_STATIC_NAME).then((cache) => {
+		fetch(e.request).then((newResp) => {
+			cache.put(e.request, newResp);
+		});
+		return cache.match(e.request);
 	});
 
-	
-
+	e.respondWith(respuesta);
+});
